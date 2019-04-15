c637b02e4f183113efa3c35d2143b69e26fb3a32
cvuuhk <36870468+cvuuhk@users.noreply.github.com>
2019-04-15 09:25:11 +0800

    完成AbsList<T>抽象类和List<T>接口的源码。

--- src/LinearList/AbsList.java
+++ src/LinearList/AbsList.java
@@ -1,5 +1,32 @@
 package LinearList;
 
+import java.util.Iterator;
+
 public abstract class AbsList<T> implements Iterable<T>, List<T>
 {
+    protected int length;
+    abstract public T get(int i);
+    abstract public void set(int i, T x);
+    abstract public int indexOf(int begin, int end, T o);
+    abstract public void add(int i, T x);
+    abstract public void clear();
+    abstract public T remove(int i);
+    abstract public Iterator<T> iterator();
+    
+    public boolean isEmpty(){return length == 0;}
+    public int length(){return length;}
+    public void add(T x){add(length, x);}
+    public void append(T x){add(length, x);}
+    public int indexOf(T o)
+    {
+        return indexOf(0, length, o);
+    }
+    public int indexOf(int begin, T o)
+    {
+        return indexOf(begin, length, o);
+    }
+    public T remove(T o)
+    {
+        return remove(indexOf(o));
+    }
 }
--- src/LinearList/List.java
+++ src/LinearList/List.java
@@ -2,4 +2,11 @@ package LinearList;
 
 public interface List<T>
 {
+    boolean isEmpty();
+    int length();
+    T get(int i);
+    void set(int i, T x);
+    void add(int i, T x);
+    T remove(int i);
+    int indexOf(T x);
 }
