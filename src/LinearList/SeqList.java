31754289ae07a38ca1061177e067f175ccd999ae
cvuuhk <36870468+cvuuhk@users.noreply.github.com>
2019-04-15 12:14:11 +0800

    添加方法声明以及注释

--- src/LinearList/SeqList.java
+++ src/LinearList/SeqList.java
@@ -1,5 +1,157 @@
 package LinearList;
 
+import java.util.Iterator;
+
 public class SeqList<T> extends AbsList<T> implements Iterable<T>
 {
+    /**
+     * 顺序表增量长度
+     */
+    private int increnentsize;
+    /**
+     * 保存顺序表数据的数组
+     */
+    protected T[] data;
+    /**
+     * 默认构造函数
+     */
+    public SeqList(){this(16);}
+    /**
+     * @param capacity 顺序表容量
+     */
+    public SeqList(int capacity){}
+    /**
+     * 构造函数, 用数组elem初始化顺序表
+     *
+     * @param elem 用来初始化顺序表
+     */
+    public SeqList(T[] elem){}
+    /**
+     * 设置顺序表每次容量增加时增量大小, 默认值是16
+     *
+     * @param inc 增量, 默认值是16
+     */
+    public void setInc(int inc){}
+    /**
+     * 设置新的容量
+     *
+     * @param newsize 新的容量
+     */
+    public void setcapacity(int newsize){}
+    /**
+     * 取得顺序表容量
+     *
+     * @return 顺序表容量
+     */
+    int getcapacity(){ return data.length;}
+    /**
+     * 取得顺序表长度, 同length()
+     *
+     * @return 顺序表长度
+     */
+    int size(){return length;}
+    /**
+     * 取得顺序表下标为i的元素值,即data[i]
+     *
+     * @param i 索引位置
+     * @return data[il
+     */
+    @Override
+    public T get(int i){return null;}
+    /**
+     * 修改下标i的元素值, 即修改data[i]的值
+     *
+     * @param i 索引位置
+     * @param x 设定值
+     */
+    @Override
+    public void set(int i, T x){}
+    private int compare(T a, T b){return 0;}
+    /**
+     * 使用顺序查找算法, 查找值为o的数据元素的下标
+     *
+     * @param begin 起始位置
+     * @param end   结束位置
+     * @param o     查找值
+     * @return 下标
+     */
+    @Override
+    public int indexOf(int begin, int end, T o){return -1;}
+    /**
+     * 内部使用,自动增加顺序表容量
+     */
+    private void grow(){}
+    /**
+     * 在位置i插入数据元素x
+     *
+     * @param i 索引位置
+     * @param x 插入的值
+     */
+    @Override
+    public void add(int i, T x){}
+    /**
+     * 以有序方式向顺序表增加数据元素x
+     *
+     * @param x 增加的数据元素
+     */
+    public void addsort(T x){}
+    /**
+     * 对顺序表排序
+     */
+    public void sort(){}
+    /**
+     * 内部使用,以有序方式插入数据x
+     *
+     * @param end 结束位置
+     * @param x   要插入的数据
+     */
+    protected void insertorder(int end, T x){}
+    /**
+     * @param i 索引位置
+     * @return 删除的元素
+     */
+    @Override
+    public T remove(int i){return null;}
+    /**
+     * 清除整个顺序表
+     */
+    @Override
+    public void clear(){}
+    public String tostring(){return "";}
+    /**
+     * 将顺序表转换为Object数组
+     *
+     * @return Object数组
+     */
+    public Object[] toArray(){return null;}
+    //todo:注释
+    /**
+     * 将顺序表转换为类型T的数组
+     *
+     * @param a
+     * @return
+     */
+    public T[] toArray(T[] a){return null;}
+    ///////////////////////////////////////////////////////////
+    @Override
+    public Iterator<T> iterator(){return new MyIterator();}
+    /**
+     * 内部类,迭代器实现原理与链表迭代器相同, 见“链表”一节解释
+     */
+    class MyIterator implements Iterator<T>
+    {
+        private int index = 0;
+        @Override
+        public boolean hasNext()
+        {
+            //在调用next()后, index自增, 确保 index 不等于 person 的长度
+            return index != length();
+        }
+        @Override
+        public T next()
+        {
+            //使用索引来获取 SeqList 中下标为 index 的项
+            return get(index++);
+        }
+    }
 }
